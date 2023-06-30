d73467ca4dfdfa3270d42b56fadddaef4fedfe7b
Lorenzo La Piana <109413745+Metaluppolo@users.noreply.github.com>
2023-06-30 23:57:14 +0200

    Implemented BookshelfComponent

--- src/app/components/bookshelf/bookshelf.component.ts
+++ src/app/components/bookshelf/bookshelf.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { BookshelfService } from 'src/app/services/bookshelf.service';
 
 @Component({
   selector: 'app-bookshelf',
   templateUrl: './bookshelf.component.html',
   styleUrls: ['./bookshelf.component.scss']
 })
-export class BookshelfComponent {
+export class BookshelfComponent implements OnInit{
+  books: any[] = [ ];
+
+  constructor(private bookshelfService: BookshelfService, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.bookshelfService.getBooks().subscribe((res: any) => {
+      const data = res.data;
+      this.books = Object.keys(data).map((key) => { 
+        return data[key];
+      });
+      //console.log(this.books)
+    });
+  }
 
 }
