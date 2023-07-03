74b4b2de040e71a275ca1aceddf648d1d7a98256
Lorenzo La Piana <109413745+Metaluppolo@users.noreply.github.com>
2023-07-03 12:40:53 +0200

    DiscoverComponent style correction

--- src/app/services/bookcard.service.ts
+++ src/app/services/bookcard.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BookModel } from '../models/book.model';
 
+const MAX_CHARS = 140;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +11,7 @@ export class BookCardService {
   constructor() { }
 
   splitPlotString(book: BookModel) {
-    const substring = book.plot.slice(0,150);
+    const substring = book.plot.slice(0, MAX_CHARS);
     book.plot_start = substring.split(' ').slice(0, -1).join(" ");
     book.plot_end = book.plot.split(book.plot_start).join(" ");
   }
